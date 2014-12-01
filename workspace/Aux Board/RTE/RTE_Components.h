9800498fc08b1e57749b32fcf68d1d66079228a6
Julien Guerinet <julien@guerinet.com>
2014-12-01 15:51:23 -0500

    Set up the aux project

--- workspace/Aux Board/RTE/RTE_Components.h
+++ workspace/Aux Board/RTE/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'RTX Base Project' 
+ * Project: 'Aux Project' 
  * Target:  'Target 1' 
  */
 
