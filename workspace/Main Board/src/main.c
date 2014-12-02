1fe207acd105fe8639599834cc7032ff50b0d1d0
jguerinet <julien@guerinet.com>
2014-12-02 09:13:23 -0500

    Refactoring Hamza's code, preparing for the multi-thread solution

--- workspace/Main Board/src/main.c
+++ workspace/Main Board/src/main.c
@@ -15,133 +15,148 @@
 #include <stdio.h>
 #include <string.h>
 
+/* SIGNALS */
+//Display Update Signal 
+#define DISPLAY_SIGNAL (int32_t) 0x01
 
-static void delay(__IO uint32_t nCount)
-{
+/* THREAD FUNCTIONS */
+//LCD
+void lcd(void const *arguments);
+
+/* THREAD DEFINITIONS */
+//LCD
+osThreadDef(lcd, osPriorityNormal, 1, 0);
+
+/* THREAD IDs */
+//LCD
+osThreadId lcd_thread;
+
+/* MUTEX DEFINTIIONS */
+//Position measured by the sensor
+osMutexDef(sensorCoordinates);
+
+/* MUTEX IDs */
+osMutexId sensorCoordinatesId;
+
+//This for the timer to update the display 
+void displayTimerCallback(void const* arg);
+osTimerDef(displayTimer, displayTimerCallback); 
+
+//This struct will contain x and y coordinates measured
+typedef struct{
+	float x; 
+	float y;
+} Coordinates; 
+
+//Helper method to simulate a delay
+static void delay(__IO uint32_t nCount){
   __IO uint32_t index = 0; 
-  for(index = 100000*nCount; index != 0; index--)
-  {
-  }
+  for(index = 100000*nCount; index != 0; index--){}
 }
-
-double x_c = 3.5;
-double y_c = 2.5;
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/**
-  * @brief    Illustartes a simple animation program
-  * @function This function draws concentrated circles emanating from the center towards the LCD edge in
-              an animated fashion. It will look as a sonar or radar display. Then it simulates locking 
-              onto a target by flashing a small red circle and displaying the text "Object located"
-  * @param    None
-  * @retval   None
-  */
-
-void locate(void const *argument){
-	while(1){
-			/* Clear the LCD */ 
-		LCD_Clear(LCD_COLOR_WHITE);
-		LCD_SetFont(&Font8x8);
-		LCD_DisplayStringLine(LINE(1), (uint8_t*)"Tracking");
-		
-		
-		int i = 0;
-		int j = 0;
-		
-		LCD_SetTextColor(LCD_COLOR_BLUE2);
-
-		
-		for (i=10; i <= 310; i = i+60){
-			LCD_DrawLine(10, i, 220, LCD_DIR_HORIZONTAL);
-		}
-
-		for (j=10; j <= 230; j = j+44){
-			LCD_DrawLine(j, 10, 300, LCD_DIR_VERTICAL);
-		}
-		
-		double x_mapped = (x_c+(10/44))*44;
-		double y_mapped = (y_c+(10/60))*60;
-		
-		if (x_mapped < 220 & y_mapped < 300){
-			LCD_SetTextColor(LCD_COLOR_RED);
-			LCD_DrawFullCircle(x_mapped,y_mapped,4);
-			delay(125);
-			LCD_SetTextColor(LCD_COLOR_WHITE);
-			LCD_DrawFullCircle(x_mapped,y_mapped,4);
-			delay(125);
-			LCD_SetTextColor(LCD_COLOR_RED);
-			LCD_DrawFullCircle(x_mapped,y_mapped,4);
-			delay(125);
-			LCD_SetTextColor(LCD_COLOR_WHITE);
-			LCD_DrawFullCircle(x_mapped,y_mapped,4);
-			delay(125);
-			LCD_SetTextColor(LCD_COLOR_RED);
-			LCD_DrawFullCircle(x_mapped,y_mapped,4);
-			delay(125);
-			LCD_SetTextColor(LCD_COLOR_WHITE);
-			LCD_DrawFullCircle(x_mapped,y_mapped,4);
-			delay(125);
-			LCD_SetTextColor(LCD_COLOR_RED);
-			LCD_DrawFullCircle(x_mapped,y_mapped,4);
-			delay(125);
-			LCD_SetTextColor(LCD_COLOR_WHITE);
-			LCD_DrawFullCircle(x_mapped,y_mapped,4);
-			delay(125);
-		}
-		else{
-			LCD_Clear(LCD_COLOR_WHITE);
-			LCD_SetFont(&Font8x8);
-			LCD_DisplayStringLine(LINE(1), (uint8_t*)"Out of Bounds");
-		}
-		
-		osDelay(250);
-	}
-}
-
-
-osThreadDef(locate, osPriorityNormal, 1, 0);
-
-// ID for theads
-
-osThreadId locate_thread;
-
-/*
- * main: initialize and start the system
- */
 
 int main (void) {
-  osKernelInitialize ();                    // initialize CMSIS-RTOS
+	//Initialize CMSIS-RTOS
+  osKernelInitialize ();                    
 	
-  // initialize peripherals here
-	/* LCD initiatization */
+	/* LCD */
+	//LCD Initialization
   LCD_Init();
-  
-  /* LCD Layer initiatization */
+  //LCD Layer Initialization
   LCD_LayerInit();
-    
-  /* Enable the LTDC controler */
+  //Enable the LTDC controller
   LTDC_Cmd(ENABLE);
-  
-  /* Set LCD foreground layer as the current layer */
+  //Set LCD foreground layer as the current layer
   LCD_SetLayer(LCD_FOREGROUND_LAYER);
 	
+	//Set up the Coordinates struct for the motion sensor 
+	//Initially everything is -1
+	Coordinates sensorCoordinates = {-1};
 	
+	/* Mutex Creations */
+	sensorCoordinatesId = osMutexCreate(osMutex(sensorCoordinates)); 
 	
-  // create 'thread' functions that start executing,
-  // example: tid_name = osThreadCreate (osThread(name), NULL);
+	/* Thread Creations */
+	lcd_thread = osThreadCreate(osThread(lcd), &sensorCoordinates);
 	
-	/*******************************************************
-	         Uncomment the example you want to see
-	example_1a: Simple shape draw, fill and text display
-	example_1b: bitmap image display
-	example_1c: Simple animation
-	********************************************************/
-	
-	//example_1a_thread = osThreadCreate(osThread(example_1a), NULL);
-	//example_1b_thread = osThreadCreate(osThread(example_1b), NULL);
-	locate_thread = osThreadCreate(osThread(locate), NULL);
-	
-	osKernelStart ();                         // start thread execution 
+	//Start thread execution
+	osKernelStart();                         
 }
 
-
+/**
+	* The LCD screen thread. 
+  */
+void lcd(void const *argument){
+	//Get the sensor coordinates from the arguments
+	Coordinates *sensorCoordinates = (Coordinates *)argument; 
+	
+	//Clear the LCD
+	LCD_Clear(LCD_COLOR_WHITE);
+	LCD_SetFont(&Font8x8);
+	
+	//Draw the grid in blue
+	LCD_SetTextColor(LCD_COLOR_BLUE2);
+	int i = 0;
+	int j = 0;
+	//Horizontal Lines
+	for (i = 10; i <= 310; i = i+60){
+		LCD_DrawLine(10, i, 220, LCD_DIR_HORIZONTAL);
+	}
+	//Vertical lines 
+	for (j = 10; j <= 230; j = j+44){
+		LCD_DrawLine(j, 10, 300, LCD_DIR_VERTICAL);
+	}
+	
+	//This will keep track of the x and y coordinates that are currently mapped 
+	double xMapped = -1; 
+	double yMapped = -1;
+	
+	//Set up the timer and start it
+	osTimerId displayTimerId = osTimerCreate(osTimer(displayTimer), osTimerPeriodic, NULL); 
+	osTimerStart(displayTimerId, 5); 
+	
+	//Main Loop
+	while(1){
+		//Wait until the display signal is set
+		osSignalWait(DISPLAY_SIGNAL, osWaitForever);
+		
+		//Get the sensor coordinates mutex
+		osMutexWait(sensorCoordinatesId, osWaitForever); 
+		
+		//Get the coordinates from this
+		double x = sensorCoordinates->x;
+		double y = sensorCoordinates->y;
+		
+		//Release the sensor coordinates mutex
+		osMutexRelease(sensorCoordinatesId); 
+		
+		//If there was a previous position, hide it
+		if(xMapped != -1){
+			LCD_SetTextColor(LCD_COLOR_WHITE);
+			LCD_DrawFullCircle(xMapped, yMapped, 4);
+		}
+		
+		//Calculate the new x and y positions to map
+		xMapped = (x+(10/44))*44;
+		yMapped = (y+(10/60))*60;
+		
+		//Show the position on the screen if it is in bounds
+		if (xMapped < 220 & yMapped < 300){
+			LCD_SetTextColor(LCD_COLOR_RED);
+			LCD_DrawFullCircle(xMapped, yMapped, 4);
+		}
+		else{
+			//Printf error message
+			printf("Out of Bounds: %f, %f", xMapped, yMapped);
+			//Set the mapped values to -1
+			xMapped = -1; 
+			yMapped = -1; 
+		}
+	}
+}
+/**
+	Handles the timer callback for updating the display
+*/
+void displayTimerCallback(void const* argument){
+	//Send a signal to the lcd thread
+	osSignalSet(lcd_thread, DISPLAY_SIGNAL); 
+}
