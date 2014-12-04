6abd5f1b332c9d5978fbdbb5d0900466c03e82b8
Julien Guerinet <julien@guerinet.com>
2014-12-04 13:16:51 -0500

    Added comments to the cc2500interface document

--- workspace/Main Board/src/cc2500Interface.c
+++ workspace/Main Board/src/cc2500Interface.c
@@ -3,6 +3,7 @@
 #include "cc2500interface.h"
 
 void initializeWireless() {
+	//Initialize the transmitter
 	CC2500_Init();
 	
 	GPIO_InitTypeDef GPIO_init_st;
@@ -20,6 +21,7 @@ void initializeWireless() {
 	GPIO_init_st.GPIO_OType = GPIO_OType_PP;
 	GPIO_Init(CC2500_SPI_INT_GPIO_PORT, &GPIO_init_st);
 	
+	//Configure the EXTI Interrupt
 	SYSCFG_EXTILineConfig(CC2500_SPI_INT_EXTI_PORT_SOURCE, CC2500_SPI_INT_EXTI_PIN_SOURCE);
 	EXTI_InitStructure.EXTI_Line = CC2500_SPI_INT_EXTI_LINE;
 	EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
@@ -27,7 +29,7 @@ void initializeWireless() {
 	EXTI_InitStructure.EXTI_LineCmd = ENABLE;
 	EXTI_Init(&EXTI_InitStructure);
 	
-	// configure NVIC
+	//Configure the NVIC for the interrupt
 	NVIC_init_st.NVIC_IRQChannel = CC2500_SPI_INT_EXTI_IRQn;
 	NVIC_init_st.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_init_st.NVIC_IRQChannelPreemptionPriority = 0x01;
